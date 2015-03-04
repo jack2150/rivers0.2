e3870537810ffae76ce0b56ce30d1383d40a2a8e
jack2150 <bvc100x@gmail.com>
2015-03-05 02:08:47 +0800

    no static

--- position/class/tests.py
+++ position/class/tests.py
@@ -8,24 +8,10 @@ from unittest import TestCase
 
 
 class TestABC(TestCase):
+
+
     def test_some(self):
-        print Statement.objects.using('default').count()
-        print Statement.objects.using('slave').count()
+        pass
 
-
-        statement = Statement(
-            date='2014-12-12',
-            account_statement='',
-            position_statement='',
-            trade_activity='',
-        )
-        statement.save(using='slave')
-
-
-
-        print Statement.objects.using('default').count()
-        print Statement.objects.using('slave').count()
-
-        Statement.objects.filter(date='2014-12-12').delete()
 
 # todo: position set start here...
